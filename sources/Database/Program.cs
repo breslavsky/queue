3b4fa3931a1d24403b268f95f29698e24969c4a2
breslavsky <anton@breslavsky.ru>
2014-09-15 15:32:50 +0400

    refactor

--- sources/Database/Program.cs
+++ sources/Database/Program.cs
@@ -6,17 +6,17 @@ using System.Windows.Forms;
 
 namespace Queue.Database
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            //Application.Run(new MainForm());
         }
     }
 }
