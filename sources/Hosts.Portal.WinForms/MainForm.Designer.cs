732d7c39bbeeae8dc286e73c892f6a0424412874
lapshin_1988 <staslapshin88@gmail.com>
2015-01-20 20:58:55 +0300

    portal form

--- sources/Hosts.Portal.WinForms/MainForm.Designer.cs
+++ sources/Hosts.Portal.WinForms/MainForm.Designer.cs
@@ -28,20 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.stopButton = new System.Windows.Forms.Button();
+            this.serviceGroupBox = new System.Windows.Forms.GroupBox();
+            this.serviceStatePicture = new System.Windows.Forms.PictureBox();
+            this.runServiceButton = new System.Windows.Forms.Button();
+            this.installServiseButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.settingsGroupBox = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.serviceGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.serviceStatePicture)).BeginInit();
+            this.settingsGroupBox.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(212, 424);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(195, 30);
+            this.stopButton.TabIndex = 15;
+            this.stopButton.Text = "Остановить сервер";
+            this.stopButton.UseVisualStyleBackColor = true;
+            // 
+            // serviceGroupBox
+            // 
+            this.serviceGroupBox.Controls.Add(this.serviceStatePicture);
+            this.serviceGroupBox.Controls.Add(this.runServiceButton);
+            this.serviceGroupBox.Controls.Add(this.installServiseButton);
+            this.serviceGroupBox.Location = new System.Drawing.Point(12, 356);
+            this.serviceGroupBox.Name = "serviceGroupBox";
+            this.serviceGroupBox.Size = new System.Drawing.Size(395, 62);
+            this.serviceGroupBox.TabIndex = 14;
+            this.serviceGroupBox.TabStop = false;
+            this.serviceGroupBox.Text = "Сервис";
+            // 
+            // serviceStatePicture
+            // 
+            this.serviceStatePicture.Location = new System.Drawing.Point(349, 20);
+            this.serviceStatePicture.Name = "serviceStatePicture";
+            this.serviceStatePicture.Size = new System.Drawing.Size(24, 24);
+            this.serviceStatePicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.serviceStatePicture.TabIndex = 2;
+            this.serviceStatePicture.TabStop = false;
+            // 
+            // runServiceButton
+            // 
+            this.runServiceButton.Location = new System.Drawing.Point(154, 19);
+            this.runServiceButton.Name = "runServiceButton";
+            this.runServiceButton.Size = new System.Drawing.Size(127, 23);
+            this.runServiceButton.TabIndex = 1;
+            this.runServiceButton.Text = "Запустить сервис";
+            this.runServiceButton.UseVisualStyleBackColor = true;
+            // 
+            // installServiseButton
+            // 
+            this.installServiseButton.Location = new System.Drawing.Point(21, 19);
+            this.installServiseButton.Name = "installServiseButton";
+            this.installServiseButton.Size = new System.Drawing.Size(127, 23);
+            this.installServiseButton.TabIndex = 0;
+            this.installServiseButton.Text = "Установить сервис";
+            this.installServiseButton.UseVisualStyleBackColor = true;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 424);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(195, 30);
+            this.startButton.TabIndex = 13;
+            this.startButton.Text = "Запустить сервер";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // settingsGroupBox
+            // 
+            this.settingsGroupBox.Controls.Add(this.groupBox1);
+            this.settingsGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.settingsGroupBox.Name = "settingsGroupBox";
+            this.settingsGroupBox.Size = new System.Drawing.Size(395, 338);
+            this.settingsGroupBox.TabIndex = 16;
+            this.settingsGroupBox.TabStop = false;
+            this.settingsGroupBox.Text = "Настройки";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(3, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(386, 134);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(395, 462);
+            this.ClientSize = new System.Drawing.Size(421, 460);
+            this.Controls.Add(this.settingsGroupBox);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.serviceGroupBox);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Портал";
+            this.serviceGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.serviceStatePicture)).EndInit();
+            this.settingsGroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.GroupBox serviceGroupBox;
+        private System.Windows.Forms.PictureBox serviceStatePicture;
+        private System.Windows.Forms.Button runServiceButton;
+        private System.Windows.Forms.Button installServiseButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.GroupBox settingsGroupBox;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
