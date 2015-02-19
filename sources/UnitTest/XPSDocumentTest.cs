b90818a99da8fa34a972cc6fecf88eb0d9ac0302
lapshin1988 <lapshin_1988@mail.ru>
2015-02-19 22:56:46 +0300

    terminal: coupon small changes

--- sources/UnitTest/XPSDocumentTest.cs
+++ sources/UnitTest/XPSDocumentTest.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Queue.Services.DTO;
+using Queue.UI.Common;
 using System;
-using System.IO;
-using System.IO.Packaging;
 using System.Printing;
-using System.Windows.Markup;
-using System.Windows.Xps.Packaging;
-using System.Windows.Xps.Serialization;
 
 namespace UnitTest
 {
@@ -13,39 +10,41 @@ namespace UnitTest
     public class XPSDocumentTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CouponXPS()
         {
-            object doc;
-
-            FileInfo fileInfo = new FileInfo("D:\\tmp\\test.xaml");
-
-            using (FileStream file = fileInfo.OpenRead())
-            {
-                ParserContext context = new System.Windows.Markup.ParserContext();
-                context.BaseUri = new Uri(fileInfo.FullName, UriKind.Absolute);
-                doc = XamlReader.Load(file, context);
-            }
-
-            string xpsFile = "D:\\tmp\\test.xps";
-
-            using (var container = Package.Open(xpsFile, FileMode.Create))
-            {
-                using (XpsDocument xpsDoc = new XpsDocument(container, CompressionOption.Maximum))
+            ClientRequestCoupon data = new ClientRequestCoupon()
                 {
-                    XpsSerializationManager rsm = new XpsSerializationManager(new XpsPackagingPolicy(xpsDoc), false);
+                    Client = new Client()
+                    {
+                        Name = "Вася"
+                    },
+                    CreateDate = DateTime.Now,
+                    IsToday = true,
+                    Number = 10,
+                    Position = 15,
+                    QueueName = "super",
+                    RequestDate = DateTime.Now,
+                    RequestTime = DateTime.Now.TimeOfDay,
+                    Service = new Service()
+                    {
+                        Name = "super service"
+                    },
+                    Subjects = 10,
+                    Workplaces = new Workplace[] { new Workplace()
+                    {
+                        Comment  = "test"
+                    }}
+                };
 
-                    rsm.SaveAsXaml(doc);
-                }
+            string xpsFile = XPSGenerator.FromXaml(Queue.Resources.Templates.ClientRequestCoupon, data);
+            try
+            {
+                PrintQueue defaultPrintQueue = LocalPrintServer.GetDefaultPrintQueue();
+                defaultPrintQueue.AddJob("test", xpsFile, false);
             }
-
-            LocalPrintServer localPrintServer = new LocalPrintServer();
-            PrintQueue defaultPrintQueue = LocalPrintServer.GetDefaultPrintQueue();
-
-            PrintSystemJobInfo xpsPrintJob = defaultPrintQueue.AddJob(xpsFile, xpsFile, false);
-
-            //XpsDocument xpsd = new XpsDocument("D:\\tmp\\test.xps", FileAccess.ReadWrite);
-            //System.Windows.Xps.XpsDocumentWriter xw = XpsDocument.CreateXpsDocumentWriter(xpsd);
-            //xw.Write("D:\\tmp\\test.jpg");
+            catch
+            {
+            }
         }
     }
 }
