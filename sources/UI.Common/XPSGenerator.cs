34dd24f80a3a3290a7bc46d03540f4aee0769e11
lapshin_1988 <staslapshin88@gmail.com>
2015-03-19 19:17:07 +0300

    xps generator

--- sources/UI.Common/XPSGenerator.cs
+++ sources/UI.Common/XPSGenerator.cs
@@ -1,7 +1,7 @@
 ﻿using System.IO;
 using System.IO.Packaging;
+using System.Printing;
 using System.Windows;
-using System.Windows.Documents;
 using System.Windows.Markup;
 using System.Windows.Xps;
 using System.Windows.Xps.Packaging;
@@ -15,24 +15,35 @@ namespace Queue.UI.Common
         {
             string xpsFile = destFile ?? Path.GetTempFileName() + ".xps";
 
+            PageMediaSize size;
+
             using (XmlReader stream = new XmlTextReader(new StringReader(template)))
             using (Package container = Package.Open(xpsFile, FileMode.Create))
             using (XpsDocument document = new XpsDocument(container, CompressionOption.SuperFast))
             {
                 FrameworkElement root = (FrameworkElement)XamlReader.Load(stream);
+
                 root.DataContext = dataContext;
 
-                FixedPage fixedPage = new FixedPage();
-                fixedPage.Children.Add(root);
+                root.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+                root.Arrange(new Rect(0, 0, root.DesiredSize.Width, root.DesiredSize.Height));
 
-                PageContent pageConent = new PageContent();
-                ((IAddChild)pageConent).AddChild(fixedPage);
+                //FixedPage fixedPage = new FixedPage();
+                //fixedPage.Children.Add(root);
 
-                FixedDocument fixedDocument = new FixedDocument();
-                fixedDocument.Pages.Add(pageConent);
+                //PageContent pageConent = new PageContent();
+                //((IAddChild)pageConent).AddChild(fixedPage);
+
+                //FixedDocument fixedDocument = new FixedDocument();
+                //fixedDocument.Pages.Add(pageConent);
 
                 XpsDocumentWriter xpsDocumentWriter = XpsDocument.CreateXpsDocumentWriter(document);
-                xpsDocumentWriter.Write(fixedDocument);
+                xpsDocumentWriter.Write(root, new PrintTicket()
+                    {
+                        //PageMediaType = PageMediaType.Plain
+                        //PageMediaSize = new PageMediaSize(PageMediaSizeName.ISOA6)
+                        PageMediaSize = new PageMediaSize(root.ActualWidth, root.ActualHeight)
+                    });
             }
 
             return xpsFile;
