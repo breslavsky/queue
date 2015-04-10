338196c7b8fc0c45e40a6adce0052d48ce845db6
breslavsky <anton@breslavsky.ru>
2015-04-10 19:49:26 +0300

    version

--- sources/Administrator/Properties/AssemblyInfo.cs
+++ sources/Administrator/Properties/AssemblyInfo.cs
@@ -1,38 +1,18 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using Queue.Common;
+using System.Reflection;
 using System.Resources;
+using System.Runtime.InteropServices;
 
-// Управление общими сведениями о сборке осуществляется с помощью 
-// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
-// связанные со сборкой.
-[assembly: AssemblyTitle("Queue Administrator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle(Product.Administrator.Name)]
+[assembly: AssemblyDescription(Product.Administrator.Description)]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Junte Ltd.")]
-[assembly: AssemblyProduct("Junte Queue System")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("Junte")]
+[assembly: AssemblyCompany(Product.Company)]
+[assembly: AssemblyProduct(Product.Name)]
+[assembly: AssemblyCopyright(Product.Copyright)]
+[assembly: AssemblyTrademark(Product.Trademark)]
 [assembly: AssemblyCulture("")]
-
-// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
-// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
-// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
 [assembly: ComVisible(false)]
-
-// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("3708e4b1-7ce8-4100-9b22-bf6e83093823")]
-
-// Сведения о версии сборки состоят из следующих четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии 
-//      Номер построения
-//      Редакция
-//
-// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
-// используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.6.0")]
-[assembly: AssemblyFileVersion("1.4.6.0")]
-[assembly: NeutralResourcesLanguageAttribute("ru")]
+[assembly: AssemblyVersion(Product.Administrator.Version)]
+[assembly: AssemblyFileVersion(Product.Administrator.Version)]
+[assembly: NeutralResourcesLanguageAttribute(Product.Language)]
---
+++ sources/Common/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace Queue.Common
+{
+    public struct Product
+    {
+        public const string Name = "Junte Queue System";
+        public const string Copyright = "Copyright ©  2015";
+        public const string Company = "Junte Ltd.";
+        public const string Trademark = "Junte";
+        public const string Language = "ru";
+
+        public struct Version
+        {
+            public const int Major = 1;
+            public const int Minor = 4;
+            public const string Full = string.Format("{0}.{1}", Major, Minor);
+        }
+
+        public struct Administrator
+        {
+            public const string Name = "Queue Administrator";
+            public const string Description = "";
+            public const int Build = 5;
+            public const string Version = string.Format("{0}.{1}", Product.Version.Full, Build);
+            public const string Guid = "3708e4b1-7ce8-4100-9b22-bf6e83093823";
+        }
+    }
+}
