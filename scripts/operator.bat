f3748abca1bdad661410822ae118532c3736fa68
Stanislav <lapshin_1988@mail.ru>
2015-04-12 18:25:10 +0300

    build script

---
+++ scripts/operator.bat
@@ -0,0 +1,2 @@
+powershell Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+powershell -File "D:\projects\queue\scripts\build.ps1" "Operator"
--- sources/Common/Product.cs
+++ sources/Common/Product.cs
@@ -12,7 +12,8 @@
         {
             public const int Major = 1;
             public const int Minor = 4;
-            public const string Full = string.Format("{0}.{1}", Major, Minor);
+
+            public const string Full = "1.4";
         }
 
         public struct Administrator
@@ -20,7 +21,7 @@
             public const string Name = "Queue Administrator";
             public const string Description = "";
             public const int Build = 5;
-            public const string Version = string.Format("{0}.{1}", Product.Version.Full, Build);
+            public const string Version = "1.4.5";
             public const string Guid = "3708e4b1-7ce8-4100-9b22-bf6e83093823";
         }
     }
